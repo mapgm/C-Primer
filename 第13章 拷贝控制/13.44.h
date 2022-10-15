16b75180b5305546a94fd55730ce7f31965221ff
riz9 <mapgm2002@163.com>
2022-10-15 10:59:27 +0800

    第13章完成

--- 第13章 拷贝控制/13.44.h
+++ 第13章 拷贝控制/13.44.h
@@ -8,16 +8,16 @@ public:
 	String(const String&); // 参数构造函数 接受string类型字符串
 	String& operator=(const String&); // 拷贝赋值运算符
 	~String(); // 析构函数
-	
+
 	const char *c_str() const { return elements; }
 	size_t size() const { return end - elements; }
 	size_t length() const { return end - elements - 1; }
-	
+
 private:
 	std::pair<char*, char*> alloc_n_copy(const char*, const char*);
 	void range_initializer(const char*, const char*);
 	void free();
-	
+
 private:
 	char *elements;
 	char *end;
